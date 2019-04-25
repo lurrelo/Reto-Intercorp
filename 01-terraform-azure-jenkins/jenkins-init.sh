0c06a146eab0a7ce2716a641cb896231cc018f1b
root <root@Robinson.localdomain>
2019-04-25 11:51:58 -0500

    updated jenkins-init.sh

--- 01-terraform-azure-jenkins/jenkins-init.sh
+++ 01-terraform-azure-jenkins/jenkins-init.sh
@@ -4,6 +4,3 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins
-usermod -aG docker jenkins
-usermod -a -G docker ubuntu
-service jenkins start
