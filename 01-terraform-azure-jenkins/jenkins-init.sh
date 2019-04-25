f9464d978c653a06a8e31c5c87f10b654b41cdfb
root <root@Robinson.localdomain>
2019-04-25 13:07:12 -0500

    updated

--- 01-terraform-azure-jenkins/jenkins-init.sh
+++ 01-terraform-azure-jenkins/jenkins-init.sh
@@ -3,4 +3,4 @@
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 echo "deb http://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
 apt-get update
-apt-get install -y jenkins=2.32.1
+apt-get install -y jenkins
